9a67a22a33f6ee5c701f7f41b4471dfdaf3e1735
kxrtiswithak <kxrtiswithak@gmail.com>
2020-10-24 13:27:44 +0100

    setup basic spring webpage

---
+++ src/test/java/com/jackandkurtis/SpotifyWebBasedApplication/SpotifyWebBasedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jackandkurtis.SpotifyWebBasedApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyWebBasedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
