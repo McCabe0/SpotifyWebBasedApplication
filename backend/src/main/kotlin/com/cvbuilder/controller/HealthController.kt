c6f75c279b6bb939932b3e5635c33f2fb43e5d76
Jack McCabe <jack.mccabe@viabledata.co.uk>
2026-03-23 20:56:35 +0000

    Add health endpoint

---
+++ backend/src/main/kotlin/com/cvbuilder/controller/HealthController.kt
@@ -0,0 +1,11 @@
+package com.cvbuilder.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HealthController {
+
+    @GetMapping("/health")
+    fun health() = mapOf("status" to "ok")
+}
